bdeaf5b99fd518896b5a6c4c79b8d873a37767a1
Echooo <Echooo@yujian.com>
2022-03-23 14:40:35 +0800

    second commit

--- src/main/java/com/yujian/git/GitTest.java
+++ src/main/java/com/yujian/git/GitTest.java
@@ -3,6 +3,7 @@ package com.yujian.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello,git!");
+        System.out.println("Hello,git2!");
     }
 }
 
